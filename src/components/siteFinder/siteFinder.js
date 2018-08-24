800e5a6454a512feea3809c5e924090d228e0874
Leon Brüwer <leon.bruewer@web.de>
2018-08-24 10:36:51 +0200

    create module

--- src/components/siteFinder/siteFinder.js
+++ src/components/siteFinder/siteFinder.js
@@ -1,1 +1,17 @@
 import './siteFinder.scss';
+
+chayns.ready.then(function() {
+    myFavoriteSite.init();
+  });
+  
+(function (myFavoriteSite, chayns, window, undefined) {
+
+    'use strict';
+    
+    myFavoriteSite.init = function init(data) {
+        
+    };
+
+    
+   
+  })((window.myFavoriteSite = {}), chayns, window);
